40b4e00504312442ff3637685dc7111464baba7d
siddhant agrawal <siddhant-agrawal01>
2025-07-16 01:12:13 +0530

    increase the db connection time

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,21 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, 
+      socketTimeoutMS: 45000, 
+      connectTimeoutMS: 30000, 
+      maxPoolSize: 10, 
+      serverSelectionRetryDelayMS: 5000, 
+      heartbeatFrequencyMS: 10000, 
+      maxIdleTimeMS: 30000, 
+    });
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error(err);
+    console.error("MongoDB connection error:", err);
     process.exit(1);
   }
 };
